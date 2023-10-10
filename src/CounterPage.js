eb295de1ac5993e632241ec6f0ab9a66dabfa960
erikakasperaviciute <erika.kasperaviciute@hotmail.lt>
2023-10-10 22:45:49 +0300

    su masyvu

--- src/CounterPage.js
+++ src/CounterPage.js
@@ -2,7 +2,10 @@ import { useState } from "react";
 
 function CounterPage() {
   const initialValue = 5;
+  const maxValue = 10;
   const [num, setNum] = useState(initialValue);
+
+  const num2Arr = [-6, -5, -3, -2, -1, 1, 2, 3, 5, 6];
 
   const numHandler = (num2) => {
     setNum(num + num2);
@@ -10,6 +13,10 @@ function CounterPage() {
 
   const resetHandler = () => {
     setNum(initialValue);
+  };
+
+  const numberColor = {
+    color: num < 5 ? "red" : "green",
   };
 
   return (
@@ -21,9 +28,23 @@ function CounterPage() {
         min="1"
         onChange={(e) => setNum(e.target.value)}
       ></input>
+      <h3 style={numberColor}>{num}</h3>
+      {num2Arr.map((number, index) => (
+        <button
+          key={index}
+          onClick={() => numHandler(number)}
+          disabled={
+            (number < 1 && num <= number * -1) ||
+            (number >= 1 && num > maxValue - number)
+          }
+        >
+          {number <= 0 ? number : "+" + number}
+        </button>
+      ))}
 
-      <h3>{num}</h3>
-      <button onClick={() => numHandler(-5)} disabled={num < 6}>
+      <button onClick={resetHandler}>Reset</button>
+
+      {/* <button onClick={() => numHandler(-5)} disabled={num < 6}>
         -5
       </button>
       <button onClick={() => numHandler(-3)} disabled={num < 4}>
@@ -34,11 +55,9 @@ function CounterPage() {
       </button>
       <button onClick={() => numHandler(-1)} disabled={num < 2}>
         -1
-      </button>
+      </button> */}
 
-      <button onClick={resetHandler}>Reset</button>
-
-      <button onClick={() => numHandler(1)} disabled={num > 9}>
+      {/* <button onClick={() => numHandler(1)} disabled={num > 9}>
         +1
       </button>
       <button onClick={() => numHandler(2)} disabled={num > 8}>
@@ -49,7 +68,7 @@ function CounterPage() {
       </button>
       <button onClick={() => numHandler(5)} disabled={num > 5}>
         +5
-      </button>
+      </button> */}
     </>
   );
 }
